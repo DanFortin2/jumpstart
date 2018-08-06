82a357c73216271bacac15f649abf4b71c8d8605
unknown <fortin_85@hotmail.com>
2018-08-05 21:25:44 -0400

    added carasoul functionality

---
+++ resources/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $('.carasoul').slick({
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplayspeed: 4000,
+    arrows: false,
+    draggable: false,
+    infinite: true,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+  });
+});
